449ae1af99eb8295179bbdfc361dc613b0a5dc6b
justin <justin0979@gmail.com>
2019-03-12 13:27:35 -0500

    add flash

--- middleware/index.js
+++ middleware/index.js
@@ -39,6 +39,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
+  req.flash('error', 'Please Login First!');
   res.redirect('/login');
 };
 
